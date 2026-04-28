3b5398f875e8485de372f2d4e118d38fd028562a
it24102733 <it24102733@my.sliit.lk>
2026-04-28 10:53:20 +0530

    added DocumentType.java

---
+++ real-estate-backend/src/main/java/com/realestate/enums/DocumentType.java
@@ -0,0 +1,11 @@
+package com.realestate.enums;
+
+public enum DocumentType {
+    SALE_AGREEMENT,
+    DEED,
+    TITLE_DEED,
+    POWER_OF_ATTORNEY,
+    PROPERTY_TAX,
+    SURVEY_PLAN,
+    OTHER
+}
