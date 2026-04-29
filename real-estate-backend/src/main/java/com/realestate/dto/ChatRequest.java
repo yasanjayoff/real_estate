dcbd484d40d676b2ae3bcfe66301dcb702664768
yasanjayoff <yasanjay.off@gmail.com>
2026-04-29 23:37:46 +0530

    chatbot upload

---
+++ real-estate-backend/src/main/java/com/realestate/dto/ChatRequest.java
@@ -0,0 +1,14 @@
+package com.realestate.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRequest {
+    @NotBlank
+    private String message;
+}
---
+++ real-estate-backend/src/main/java/com/realestate/dto/ChatResponse.java
@@ -0,0 +1,12 @@
+package com.realestate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatResponse {
+    private String reply;
+}
